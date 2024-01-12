694b8dabf29526d2957f002ea8ff42d544aaeb81
Gaspard Petit <gaspardpetit@gmail.com>
2024-01-11 23:25:20 -0500

    Added document writing code

---
+++ verbatim/transcript_writing/__init__.py
@@ -0,0 +1,3 @@
+from .write_transcript import WriteTranscript
+from .write_transcript_docx import WriteTranscriptDocx
+from .wrtie_transcript_ass import WriteTranscriptAss
---
+++ verbatim/transcript_writing/write_transcript.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+from verbatim.transcription import Transcription
+
+
+class WriteTranscript(ABC):
+    """
+    Abstract base class for writing transcriptions to a file.
+
+    Attributes:
+        None
+
+    Methods:
+        execute(transcript: Transcription, output_file: str, **kwargs: dict) -> None:
+            Abstract method for executing the transcription writing process.
+
+    """
+
+    @abstractmethod
+    def execute(self, transcript: Transcription, output_file: str, **kwargs: dict) -> None:
+        """
+        Execute the transcription writing process.
+
+        Args:
+            transcript (Transcription): The transcription to be written.
+            output_file (str): The path to the output file.
+            **kwargs (dict): Additional keyword arguments for customization.
+
+        Returns:
+            None
+
+        Raises:
+            NotImplementedError: This method must be implemented by the derived class.
+        """
+        pass
