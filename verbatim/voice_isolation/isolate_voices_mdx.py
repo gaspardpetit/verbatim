b836bdba79641aff4f1e71efb855e9c6d3597c11
Gaspard Petit <gaspardpetit@gmail.com>
2024-01-12 01:26:27 -0500

    When splitting voice and background, save both artifacts for audit

--- verbatim/voice_isolation/isolate_voices_mdx.py
+++ verbatim/voice_isolation/isolate_voices_mdx.py
@@ -2,7 +2,7 @@ from audio_separator.separator import Separator
 from .isolate_voices import IsolateVoices
 from ..wav_conversion import ConvertToWav
 from numpy import ndarray
-
+import shutil
 
 class IsolateVoicesMDX(IsolateVoices):
     """
@@ -33,8 +33,14 @@ class IsolateVoicesMDX(IsolateVoices):
         # Use MDX to separate vocals from the source audio
         output_file_paths = separator.separate(source_path)
 
+        # Move the generated files to the output directory
+        instrument_audio = output_file_paths[0]
+        voice_audio = output_file_paths[1]
+        shutil.move(instrument_audio, f"{destination_path}-noise.wav")
+        shutil.move(voice_audio, f"{destination_path}-voice.wav")
+
         # Load the separated vocals as a NumPy array
-        waveform: ndarray = ConvertToWav.load_float32_16khz_mono_audio(output_file_paths[1])
+        waveform: ndarray = ConvertToWav.load_float32_16khz_mono_audio(f"{destination_path}-voice.wav")
 
         # Save the isolated vocals to the destination path
         ConvertToWav.save_float32_16khz_mono_audio(waveform, destination_path)
