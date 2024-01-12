8780477773e103e8a254a960510b1576e71da0e6
Gaspard Petit <gaspardpetit@gmail.com>
2024-01-11 23:29:57 -0500

    Added test

---
+++ tests/test_pipeline.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+if __name__ == "__main__":
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from verbatim.pipeline import Pipeline
+from verbatim.context import Context
+
+
+if __name__ == '__main__':
+    import logging
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s [%(levelname)s][%(filename)s:%(lineno)d][%(funcName)s] %(message)s',
+                        datefmt='%Y-%m-%dT%H:%M:%SZ')
+
+    print(os.getcwd())
+    context: Context = Context(source_file="tests/data/test.mp3", languages=["en", "fr", "es", "it"], nb_speakers=4)
+    Pipeline(context).execute()
