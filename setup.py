41a05074fdb548049e60e05a414c3072603ad161
Gaspard Petit <gaspardpetit@gmail.com>
2024-01-12 21:33:26 -0500

    Require Python 3.9 and up

--- setup.py
+++ setup.py
@@ -62,8 +62,6 @@ setup(
         "Operating System :: OS Independent",
 
         'Programming Language :: Python :: 3',
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
