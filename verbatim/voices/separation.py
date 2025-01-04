2db194e80201b6ee612ff1f81763be9036266f39
Gaspard Petit <gaspardpetit@gmail.com>
2025-01-04 16:55:26 -0500

    Move AMI diarization/separation to its own file

--- verbatim/voices/separation.py
+++ verbatim/voices/separation.py
@@ -1,11 +1,15 @@
 import logging
 from typing import Union
 
+import numpy as np
+import scipy.io.wavfile
 import torch
 from pyannote.audio import Pipeline
 from pyannote.audio.pipelines.utils.hook import ProgressHook
 from pyannote.core.annotation import Annotation
 from pyannote.database.util import load_rttm
+
+from ..audio.audio import wav_to_int16
 
 # Configure logger
 LOG = logging.getLogger(__name__)
@@ -15,11 +19,26 @@ class Diarization:
         LOG.info("Initializing Diarization Pipeline.")
         self.huggingface_token = huggingface_token
         self.pipeline = Pipeline.from_pretrained(
-            checkpoint_path="pyannote/speaker-diarization-3.1",
+            "pyannote/speech-separation-ami-1.0",
             use_auth_token=self.huggingface_token
         )
-        self.pipeline.instantiate({})
+        hyper_parameters =         {
+                "segmentation": {
+                "min_duration_off": 0.0,
+                "threshold": 0.82
+                },
+                "clustering": {
+                    "method": "centroid",
+                    "min_cluster_size": 15,
+                    "threshold": 0.68,
+                },
+                "separation": {
+                    "leakage_removal": True,
+                    "asr_collar": 0.32,
+                }
+            }
 
+        self.pipeline.instantiate(hyper_parameters)
         self.pipeline.to(torch.device(device))
 
     @staticmethod
@@ -33,10 +52,21 @@ class Diarization:
         if not out_rttm_file:
             out_rttm_file = "out.rttm"
 
+        sources = None
         with ProgressHook() as hook:
-            diarization = self.pipeline(file_path, hook=hook, num_speakers=nb_speakers)
+            diarization, sources = self.pipeline(file_path, hook=hook)
 
         # dump the diarization output to disk using RTTM format
         with open(out_rttm_file, "w", encoding="utf-8") as rttm:
             diarization.write_rttm(rttm)
+
+        # dump sources to disk as SPEAKER_XX.wav files
+        for s, speaker in enumerate(diarization.labels()):
+            if s < sources.data.shape[1]:
+                speaker_data = sources.data[:, s]
+                if speaker_data.dtype != np.int16:
+                    speaker_data = wav_to_int16(speaker_data)
+                scipy.io.wavfile.write(f'{speaker}.wav', 16000, speaker_data)
+            else:
+                LOG.debug(f"Skipping speaker {s} as it is out of bounds.")
         return diarization
