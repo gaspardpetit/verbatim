f576172bd0e1dd52cf8414396e1a5197045db410
Gaspard Petit <gaspardpetit@gmail.com>
2025-01-04 15:08:37 -0500

    Updated Dockerfile

--- docker-build.sh
+++ docker-build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Check if the environment variable is set
-if [ -z "$TOKEN_HUGGINGFACE" ]; then
-    echo "Error: TOKEN_HUGGINGFACE environment variable is not set."
+if [ -z "$HUGGINGFACE_TOKEN" ]; then
+    echo "Error: HUGGINGFACE_TOKEN environment variable is not set."
     exit 1
 fi
 
-echo docker build --build-arg="TOKEN_HUGGINGFACE=$TOKEN_HUGGINGFACE" -t verbatim .
-docker build --build-arg="TOKEN_HUGGINGFACE=$TOKEN_HUGGINGFACE" -t verbatim .
+echo docker build --build-arg="HUGGINGFACE_TOKEN=$HUGGINGFACE_TOKEN" -t verbatim .
+docker build --build-arg="HUGGINGFACE_TOKEN=$HUGGINGFACE_TOKEN" -t verbatim .
 
