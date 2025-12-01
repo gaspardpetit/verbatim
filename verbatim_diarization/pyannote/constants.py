6f36b9b8c53e76122283f38a4d0987e4fbee3563
Gaspard Petit <gaspardpetit@gmail.com>
2025-12-01 10:38:12 -0500

    pin models

--- verbatim_diarization/pyannote/constants.py
+++ verbatim_diarization/pyannote/constants.py
@@ -1,7 +1,7 @@
 """Shared defaults for pyannote diarization/separation models."""
 
 PYANNOTE_DIARIZATION_MODEL_ID = "pyannote/speaker-diarization-community-1"
-PYANNOTE_DIARIZATION_MODEL_REVISION = "refs/heads/main"
+PYANNOTE_DIARIZATION_MODEL_REVISION = "3533c8cf8e369892e6b79ff1bf80f7b0286a54ee"
 
 PYANNOTE_SEPARATION_MODEL_ID = "pyannote/speech-separation-ami-1.0"
-PYANNOTE_SEPARATION_MODEL_REVISION = "refs/heads/main"
+PYANNOTE_SEPARATION_MODEL_REVISION = "9486b106945ae0cc0784041a08bfcdba5edadfb9"
