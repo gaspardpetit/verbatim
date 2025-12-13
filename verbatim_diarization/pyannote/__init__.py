dfefc70a29ff5c76069168c6b23607933fa6eda0
Gaspard Petit <gaspardpetit@gmail.com>
2025-12-13 17:44:27 -0500

    DIsable pyannote metrics causing warnings in the logs

--- verbatim_diarization/pyannote/__init__.py
+++ verbatim_diarization/pyannote/__init__.py
@@ -1,3 +1,8 @@
+import os
+
+# Disable pyannote telemetry unless caller opts in explicitly via env.
+os.environ.setdefault("PYANNOTE_METRICS_ENABLED", "false")
+
 from .diarize import PyAnnoteDiarization, PyAnnoteSeparationDiarization
 from .separate import PyannoteSpeakerSeparation
 
